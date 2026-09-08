36bc0ec986b4dd70cb36c0f0e1b0e41995748443
Ana Clara Mesquita <164940221+anamesquuita@users.noreply.github.com>
2026-09-08 10:45:48 -0300

    Update hurdles_1.py

--- Dia06/hurdles_1.py
+++ Dia06/hurdles_1.py
@@ -2,14 +2,17 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
-    
-while not at_goal():
-    if right_is_clear():
-        turn_right()
-        move()
-    elif front_is_clear():
-        move()
-    else:
-        turn_left()
 
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+for step in range(6):
+    jump()
 
