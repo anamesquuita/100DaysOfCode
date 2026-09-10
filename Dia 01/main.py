d111f24035305afe98f25279e64320df93cfd1f6
Ana Clara Mesquita <164940221+anamesquuita@users.noreply.github.com>
2026-09-10 18:21:21 -0300

    Create main.py

---
+++ Dia 01/main.py
@@ -0,0 +1,6 @@
+print("Gere um nome para sua banda!")
+city=input("Qual o nome da sua cidade natal?\n")
+pet=input("Qual o nome do seu pet?\n")
+print("O nome da sua banda poderia ser: "+ city + " " + pet)
+
+input("\nPressione ENTER para sair...")
